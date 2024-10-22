aae9764753ee8940c28f8c2751efa3926b441469
brian <yangbri1@msu.edu>
2024-10-22 17:46:47 -0400

    created GET route to access all of the documents in Character collection

--- routes/characterRoutes.mjs
+++ routes/characterRoutes.mjs
@@ -6,16 +6,39 @@ import Character from '../models/characterSchema.mjs';
 const router = express.Router();
 
 // testing route
-router.get('/', (req, res) => {
-    res.send(`Testing routes`);
+// router.get('/', (req, res) => {
+//     res.send(`Testing routes`);
+// });
+
+// 1. specify collection
+// 2. specify action
+// 3. return results
+// 4. wrap in try-catch to handle any extraneous errors
+
+/* Create */
+
+/* Read */
+// 
+router.get('/', async (req, res) => {
+    // try-catch error handling to handle any possible extraneous errors
+    try {
+        // no need to specify collection with Mongoose as it's done elsewhere
+        // call Mongoose .find() function with empty object {} to query all documents from Character collection
+        const allCharacters = await Character.find({});
+
+        // return all characters in JSON string format to client
+        res.json({});
+        
+    } catch (err) {
+        // console out error in command line interface
+        console.error(err);
+        // output custom error "500" and message to 
+        res.status(500).json({msg: "Internal Server Error - GET"});
+    }
 });
 
-// Create
+/* Update */
 
-// Read
-
-// Update
-
-// Delete
+/* Delete */
 
 export default router;
