38b2631eb55b07576dedccd5b922fc2b39bd0081
brian <yangbri1@msu.edu>
2024-10-23 23:59:09 -0400

    creating devil_fruit_database data collection

---
+++ data/devil_fruit_data.mjs
@@ -0,0 +1,83 @@
+// devil fruits (Akuma no Mi) database
+export const devil_fruit = [
+    {
+        name: "Brain-Brain Fruit",
+        type: "Paramecia",
+        ability: "User's brain expand boundlessly with knowledge",
+        user: "Vegapunk",
+        awakened: false
+    },
+    {
+        name: "Dice-Dice Fruit",
+        type: "Paramecia",
+        ability: "Enables user's body to become as sharp as a steel blade",
+        user: "Daz Bonez",
+        awakened: false
+    },
+    {
+        name: "Human-Human Fruit",
+        type: "Mythical Zoan",
+        ability: "Allow transformation into 'Sun God' Nika",
+        user: "Joy Boy",
+        awakened: true
+    },
+    {
+        name: "Cat-Cat Fruit",
+        type: "Zoan",
+        user: "Rob Lucci",
+        awakened: true
+    },
+    {
+        name: "Gol-Gol Fruit",
+        type: "Paramecia",
+        user: "Gild Tesoro",
+        awakened: true
+    },
+    {
+        name: "Dark-Dark Fruit",
+        type: "Logia",
+        user: "Marshall D. Teach",
+        awakened: false
+    },
+    {
+        name: "Tremor-Tremor Fruit",
+        type: "Paramecia",
+        ability: "User can create vibrations or quakes",
+        user: "Edward Newgate",
+        awakened: false
+    },
+    {
+        name: "Snake-Snake Fruit",
+        type: "Mythical Zoan",
+        user: "Kurozumi Orochi",
+        awakened: false
+    },
+    {
+        name: "Barrier-Barrier Fruit",
+        type: "Paramecia",
+        ability: "User can generate unbreakable barriers",
+        user: "Bartolomeo",
+        awakened: false
+    },
+    {
+        name: "Time-Time Fruit",
+        type: "Paramecia",
+        ability: "User can create a timeskip for themselves and others",
+        user: "Kouzuki Toki",
+        awakened: false
+    },
+    {
+        name: "Flame-Flame Fruit",
+        type: "Logia",
+        ability: "User can control, create, transform fire",
+        user: "Portgas D. Ace",
+        awakened: false
+    },
+    {
+        name: "Calm-Calm Fruit",
+        type: "Paramecia",
+        ability: "User can create soundproof field and cancel all noises",
+        user: "Donquixote Rosinante",
+        awakened: false
+    },
+]
