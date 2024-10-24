a1a950ada8f3c82e68b79775c0b5cee7e64be8e6
brian <yangbri1@msu.edu>
2024-10-24 01:46:25 -0400

    put together a boilerplate for the devilFruitRoutes.mjs to construct routes for both CRUD and established static methods for model in the devilFruitSchema

---
+++ routes/devilFruitRoutes.mjs
@@ -0,0 +1,21 @@
+// import relevant modules to be used
+import express from 'express';
+import Devil_Frut from '../models/devilFruitSchema.mjs';
+
+// create an instance of Express' Router
+const router = express.Router();
+
+// 1. specify collection (not needed as done already in schema -- SOC)
+// 2. specify action
+// 3. return results
+// 4. wrap in try-catch to handle errors
+
+/* CREATE */
+
+/* READ */
+
+/* UPDATE */
+
+/* DELETE */
+
+export default router;
